dd8bec0bb6154d7dde39d7823cb0373ad4aa7fa7
António Quaresma <antqua@outlook.pt>
2023-03-14 20:05:28 +0000

    selecting html elements in the DOM

---
+++ app.js
@@ -0,0 +1,9 @@
+// Select elements
+const display = document.querySelector('.display');
+const clearBtn = document.querySelector('.clear');
+const backspaceBtn = document.querySelector('.backspace');
+const equalBtn = document.querySelector('.equal');
+const operatorBtns = document.querySelectorAll('.operator');
+const numBtns = document.querySelectorAll(
+  '.button:not(.operator):not(.clear):not(.backspace):not(.equal)'
+);
