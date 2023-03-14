8063a3feec9c6d699e32f3d6c1c05c1760af5cc4
António Quaresma <antqua@outlook.pt>
2023-03-14 20:09:34 +0000

    function / eventListeners for numbers  /operators

--- app.js
+++ app.js
@@ -14,3 +14,47 @@ let currentNumber = '';
 let currentOperation = null;
 let previousOperationResult = null;
 //let result = null;
+
+
+function appendToDisplay(value) {
+    if (value === '.' && currentNumber.includes('.')) {
+      // If the user tries to input more than one decimal point, ignore the input
+      return;
+    }
+  
+    if (isNaN(value) && value !== '.') {
+      // Operator button clicked
+      if (currentOperation !== null) {
+        // An operation has already been chosen, so calculate the result
+        operate();
+      }
+      currentOperation = value;
+      if (previousOperationResult !== null) {
+        firstNumber = previousOperationResult;
+      } else {
+        firstNumber = parseFloat(currentNumber);
+      }
+      currentNumber = '';
+    } else {
+      // Digit or decimal button clicked
+      currentNumber += value;
+    }
+    display.innerText = currentNumber;
+  }
+
+
+
+
+numBtns.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      let value = event.target.dataset.value;
+      appendToDisplay(value);
+    });
+  });
+  
+  operatorBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+      let operator = button.getAttribute('data-operator');
+      appendToDisplay(operator);
+    });
+  });
